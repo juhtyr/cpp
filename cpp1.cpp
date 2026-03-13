b9baefdd3d6d1e58afaab259802473686a8ab947
yujin2 <gnani1378@naver.com>
2026-03-13 16:22:54 +0900

    Add files via upload

---
+++ cpp1.cpp
@@ -0,0 +1,32 @@
+////#include<iostream>
+////
+////int main()
+////
+////
+////{
+////	int sum = 0;
+////	for (int i = 0; i <= 10; i++) {
+////
+////		sum += i;
+////	}
+////	std::cout << "1에서 10까지 더한 결과는"<< sum<<"입니다."<<"\n";
+////
+////	char c = '#';
+////	std::cout << c << 5.5 << '-' << "HI";
+////	return 0;
+////}
+////
+//
+//
+//#include<iostream>
+//
+//int main()
+//{
+//	for (int i = 0; i < 5; i++)
+//	{
+//		for (int j = 0; j < i; j++) {
+//			std::cout << "*";
+//		}
+//		std::cout << "\n";
+//	}
+//}
